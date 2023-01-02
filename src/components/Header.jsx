1622742c7ad8c25ff40eb447d917e4656c23980e
Alan Yeverino <lanyev@gmail.com>
2023-01-02 14:41:29 -0600

    styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,10 @@ import "../styles/Header.css";
 const Header = () => {
   const nameTrainer = useSelector((state) => state.nameTrainer);
 
+  const handleClickLogout = () => {
+    localStorage.removeItem("nameTrainer");
+  };
+
   return (
     <header className="header">
       <h1 className="title">
@@ -14,6 +18,11 @@ const Header = () => {
         Welcome <span className="trainer-name">{nameTrainer}</span>, here you
         can see all the pokemons you can catch!
       </p>
+      <div className="logout__container">
+        <button onClick={handleClickLogout} className="logout__btn">
+          <a href="/">Logout</a>
+        </button>
+      </div>
     </header>
   );
 };
